059519d9354270e775e81c147d9837f853047fc9
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2025-05-18 18:58:02 +0200

    created findAllByKeys function and covered it with tests

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { flattenObject } from './flattenObject';
 export { unflattenObject } from './unflattenObject';
+export {countOccurrences} from './countOccurrences'
+export {findKeyDeep} from './findKeyDeep'
+export {getAllValues} from './getAllValues'
+export {getAllKeys} from './getAllKeys'
+export {groupBy} from './groupBy'
+export {findAllByKey} from './findAllByKey'
